1ce306905173b130f393b2f41eea810070b4eddf
Rodrigo De Souza Silva <neidianerodrigo@terra.com.br>
2019-11-10 15:39:25 -0300

    correção de pacotes e referencias

---
+++ DLL/AdapterStructural/Interfaces/IChart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AdapterStructural.Interfaces
+{
+    public interface IChart
+    {
+        string Title { get; set; }
+        List<string> XData { get; set; }
+        List<string> YData { get; set; }
+        Bitmap GeneralChart { get; set; }
+    }
+}
---
+++ DLL/AdapterStructural/Readme.txt
@@ -0,0 +1,4 @@
+﻿ADAPTER
+Permite trabalhar com classes que tenham design imcompativel.
+
+O ADAPTER converte a interface de uma classe em outra interface que é esperada por uma aplicação cliente.
